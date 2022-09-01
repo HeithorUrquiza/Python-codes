dd3e62875b5df107f0d50352078896f888d9241f
Heithor Urquzia <heithoru@gmail.com>
2022-09-01 14:56:08 -0300

    Update TesteBig-O.py

--- Códigos/TesteBig-O.py
+++ Códigos/TesteBig-O.py
@@ -1,7 +1,19 @@
-lista = [1, 2, 3, 4, 5]
+import time
 
-for i in lista:
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+a = lista.index(1)
+print(a)
+'''teste = 10000
+start = time.time()
+
+for i in range(teste):
     work = 0
-    for j in lista:
+    for j in range(teste):
         work += 1
         work -= 1
+    end = time.time()
+    print("tempo: {:.2f}".format(end - start))''' 
+    
+
+
+
